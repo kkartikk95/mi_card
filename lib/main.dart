f7abdeb24f0a572fac53bcba8643359d8d4a3473
Kartikeyan Venkatasubramanian <87036022+kkartikk95@users.noreply.github.com>
2023-08-17 16:46:44 -0400

    Finished Mi card app

--- lib/main.dart
+++ lib/main.dart
@@ -44,50 +44,49 @@ class MyApp extends StatelessWidget {
                     letterSpacing: 5,
                   ),
                 ),
-                Container(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                  padding: EdgeInsets.all(10),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.call_rounded,
-                        color: Colors.teal,
-                        size: 30,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        '+1 302-384-5264',
-                        style: TextStyle(
-                            fontSize: 20, color: Colors.teal.shade900),
-                      )
-                    ],
+                SizedBox(
+                  height: 20,
+                  width: 150,
+                  child: Divider(
+                    color: Colors.teal.shade200,
                   ),
                 ),
-                Container(
+                Card(
                   color: Colors.white,
                   margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                  padding: EdgeInsets.all(10),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.email_rounded,
-                        color: Colors.teal,
-                        size: 30,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.call_rounded,
+                      color: Colors.teal,
+                      size: 30,
+                    ),
+                    title: Text(
+                      '+1 (302)-384-5264',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.teal.shade900,
                       ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        'vkart995@gmail.com',
-                        style: TextStyle(
-                            fontSize: 20, color: Colors.teal.shade900),
-                      )
-                    ],
+                    ),
                   ),
-                )
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email_rounded,
+                      color: Colors.teal,
+                      size: 30,
+                    ),
+                    title: Text(
+                      'vkart995@gmail.com',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
